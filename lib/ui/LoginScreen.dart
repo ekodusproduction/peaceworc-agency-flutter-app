ad843395608183c48d906ccf2e233a1662420ada
subhrajitdeb54 <subhrajit.deb@ekodusinc.com>
2023-09-22 18:09:37 +0530

    login validation - 22/9/2023

--- lib/ui/LoginScreen.dart
+++ lib/ui/LoginScreen.dart
@@ -14,10 +14,13 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen> with InputValidationMixin{
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
   var fcmToken = "fcm_token";
+  bool isLoading = false;
+  bool isObscured = true;
+  final formGlobalKey = GlobalKey < FormState > ();
   //late FirebaseMessaging _firebaseMessaging;
 
   @override
@@ -58,10 +61,16 @@ class _LoginScreenState extends State<LoginScreen> {
 */
 
   void login(String email, String password, String fcmToken){
+    setState(() {
+      isLoading = true;
+    });
     loginBloc.login(email, password, fcmToken);
   }
   void addLoginListener() {
     loginBloc.subject.stream.listen((value) {
+      setState(() {
+        isLoading = false;
+      });
       if(value.error == null){
         if (value.success == true) {
           Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
@@ -82,116 +91,152 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Padding(
+      body: isLoading ? Center(child: CircularProgressIndicator(color: Colors.white,)) : Padding(
         padding: const EdgeInsets.all(10.0),
         child: Stack(
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.asset("assets/images/new_logo.png")
-                ),
-                Text(
-                  'Welcome Back',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24
+            Form(
+              key: formGlobalKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: 100,
+                      height: 100,
+                      child: Image.asset("assets/images/new_logo.png")
                   ),
-                ),
-                Text(
-                  'Login to continue',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16
-                  ),
-                ),
-                SizedBox(height: 30,),
-                SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextField(
-                        controller: emailController,
-                        style: TextStyle(color: Colors.white),
-                        obscureText: false,
-                        decoration: InputDecoration(
-                            hintStyle: TextStyle(color: Colors.white),
-                            labelText: 'Email Address',
-                            labelStyle: TextStyle(color: Colors.white)
-                        ),
-                      ),
-                      SizedBox(height: 10,),
-                      TextField(
-                        controller: passwordController,
-                        style: TextStyle(color: Colors.white),
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            labelText: 'Password',
-                            labelStyle: TextStyle(color: Colors.white)
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Text(
-                  'Forgot password?',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16
-                  ),
-                ),
-                SizedBox(height: 30,),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      child: Text(
-                          "Log In",
-                          style: TextStyle(fontSize: 18, color: Colors.black)
-                      ),
-                      style: ButtonStyle(
-                          padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.only(left: 40, right: 40, top: 13, bottom: 13)),
-                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                          backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  side: BorderSide(color: Colors.white)
-                              )
-                          )
-                      ),
-                      onPressed: () => {
-                        login(emailController.text, passwordController.text, fcmToken)
-                      }
-                  ),
-                ),
-                SizedBox(height: 30,),
-                Text(
-                  'Do not have an account?',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15
-                  ),
-                ),
-                InkWell(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpScreen()));
-                  },
-                  child: Text(
-                    'Register now',
+                  Text(
+                    'Welcome Back',
                     style: TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
+                        fontSize: 24
+                    ),
+                  ),
+                  Text(
+                    'Login to continue',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          controller: emailController,
+                          style: TextStyle(color: Colors.white),
+                          obscureText: false,
+                          decoration: InputDecoration(
+                              hintStyle: TextStyle(color: Colors.white),
+                              labelText: 'Email Address',
+                              labelStyle: TextStyle(color: Colors.white)
+                          ),
+                          validator: (String? value) {
+                            if(!isEmailValidate(value!)){
+                              return 'Invalid email address';
+                            }
+                            return null;
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                        ),
+                        SizedBox(height: 10,),
+                        TextFormField(
+                          controller: passwordController,
+                          style: TextStyle(color: Colors.white),
+                          obscureText: isObscured,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(color: Colors.white),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                  isObscured ? Icons.visibility
+                                      : Icons.visibility_off),
+                              color: Colors.white,
+                              onPressed: () {
+
+                                setState(
+                                      () {
+                                        isObscured = !isObscured;
+                                  },
+                                );
+                              },
+                            ),
+                            errorMaxLines: 3,
+                          ),
+                          validator: (String? value) {
+                            if(isPasswordValid(value!).length != 0){
+                              return isPasswordValid(value!);
+                            }
+                            return null;
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Text(
+                    'Forgot password?',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        child: Text(
+                            "Log In",
+                            style: TextStyle(fontSize: 18, color: Colors.black)
+                        ),
+                        style: ButtonStyle(
+                            padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.only(left: 40, right: 40, top: 13, bottom: 13)),
+                            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    side: BorderSide(color: Colors.white)
+                                )
+                            )
+                        ),
+                        onPressed: () =>
+                        {
+                          if(formGlobalKey.currentState!.validate()){
+                            formGlobalKey.currentState!.save(),
+                            login(emailController.text, passwordController.text, fcmToken)
+                          }
+                        }
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Text(
+                    'Do not have an account?',
+                    style: TextStyle(
+                        color: Colors.white,
                         fontSize: 15
                     ),
                   ),
-                ),
-              ],
+                  InkWell(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpScreen()));
+                    },
+                    child: Text(
+                      'Register now',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
             Positioned(
               top: 40,
@@ -209,3 +254,25 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 }
+
+mixin InputValidationMixin{
+  bool isEmailValidate(String value){
+    if(!RegExp(r"^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$")
+        .hasMatch(value!)){
+      return false;
+    }
+    return true;
+  }
+
+  String isPasswordValid(String password){
+    if(password.length < 6){
+      return "Password must be consisted of 6 or more character";
+    }
+    if(!RegExp(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,12}$").hasMatch(password!)){
+      return "Password should contain at least one lower case, one upper case, one numeric value and one special character";
+    }
+    return "";
+  }
+}
+
+
